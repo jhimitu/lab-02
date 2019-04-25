d5b0d9daaf425285fc02f03074debfe0133b213a
Jhia Turner <jhimitu@gmail.com>
2019-04-24 21:19:39 -0700

    remove console logs

--- js/app.js
+++ js/app.js
@@ -4,16 +4,13 @@ let allImgObjs = [];
 let keywords = [];
 
 $.get('../data/page-1.json').done(data => {
-  console.log(data);
   data.forEach(element => {
     new Img(element.image_url, element.title, element.description, element.keyword, element.horns);
   });
-  console.log(allImgObjs);
   allImgObjs.forEach((img) => {
-    console.log(img);
     let template = $('section');
     let clone = template.clone();
-    console.log(clone);
+
     clone.children('h2').text(img.title);
     clone.children('img').attr('src', `${img.image_url}`);
     clone.children('img').attr('alt', `${img.title}`);
@@ -24,11 +21,11 @@ $.get('../data/page-1.json').done(data => {
       keywords.push(img.keyword);
     }
   });
-  console.log(keywords);
+
   keywords.forEach((keyword) => {
     let template = $('option');
     let clone = template.clone();
-    console.log(clone[0]);
+
     clone.attr('value', keyword);
     clone.text(keyword);
     $('select').append(clone[0]);
