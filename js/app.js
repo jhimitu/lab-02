88d2f5f94e078e2e2ed0460fb5bdd26707819739
Jhia Turner <jhimitu@gmail.com>
2019-04-25 20:53:36 -0700

    load page one images on page load and put get requests into functions

--- js/app.js
+++ js/app.js
@@ -1,135 +1,19 @@
 'use-strict';
 
+$(() => {
+  getPageOne();
+});
+
+
 $('ul').on('click', (e) => {
   console.log(e);
   if(e.target.text === 'Page 1') {
-    let allImgObjs = [];
-    let keywords = ['all'];
-
-    function Img(image_url, title, description, keyword, horns) {
-      this.image_url = image_url;
-      this.title = title;
-      this.description = description;
-      this.keyword = keyword;
-      this.horns = horns;
-      allImgObjs.push(this);
-    }
-
-    console.log('Page 1 was clicked');
-    $.get('./data/page-1.json').done(data => {
-      data.forEach(element => {
-        new Img(element.image_url, element.title, element.description, element.keyword, element.horns);
-      });
-      console.log('image objects: ', allImgObjs);
-      console.log('keywords: ', keywords);
-
-      let template = $('#photo-template');
-      $('main').empty();
-      $('main').append(template[0]);
-      console.log(template);
-  
-      let optionTemplate = $('option');
-      $('select').empty();
-      $('select').append(optionTemplate[0]);
-      console.log(optionTemplate);
-
-      //creates a new section for each image
-      allImgObjs.forEach((img) => {
-        let template = $('section');
-        let clone = template.clone();
-    
-        clone.attr('id', `${img.keyword}`);
-        clone.attr('class', 'all');
-        clone.children('h2').text(img.title);
-        clone.children('img').attr('src', `${img.image_url}`);
-        clone.children('img').attr('alt', `${img.title}`);
-        clone.children('p').text(img.description);
-        $('main').append(clone[0]);
-        clone.show();
-        
-        //adds each keyword to a list
-        if(!keywords.includes(img.keyword)) {
-          keywords.push(img.keyword);
-        }
-      });
-    
-      //creates a select option for each keyword from the list
-      keywords.forEach((keyword) => {
-        let template = $('option');
-        let clone = template.clone();
-    
-        clone.attr('value', keyword);
-        clone.text(keyword);
-        $('select').append(clone[0]);
-      });
-    });
+    getPageOne();
   }
   if(e.target.text === 'Page 2') {
-    console.log('Page 2 was clicked');
-    let allImgObjs = [];
-    let keywords = ['all'];
-
-    function Img(image_url, title, description, keyword, horns) {
-      this.image_url = image_url;
-      this.title = title;
-      this.description = description;
-      this.keyword = keyword;
-      this.horns = horns;
-      allImgObjs.push(this);
-    }
-
-    $.get('./data/page-2.json').done(data => {
-      data.forEach(element => {
-        new Img(element.image_url, element.title, element.description, element.keyword, element.horns);
-      });
-      console.log('image objects: ', allImgObjs);
-      console.log('keywords: ', keywords);
-
-      let template = $('#photo-template');
-      $('main').empty();
-      $('main').append(template[0]);
-      console.log(template);
-  
-      let optionTemplate = $('option');
-      $('select').empty();
-      $('select').append(optionTemplate[0]);
-      console.log(optionTemplate);
-
-
-      //creates a new section for each image
-      allImgObjs.forEach((img) => {
-        let template = $('section');
-        let clone = template.clone();
-    
-        clone.attr('id', `${img.keyword}`);
-        clone.attr('class', 'all');
-        clone.children('h2').text(img.title);
-        clone.children('img').attr('src', `${img.image_url}`);
-        clone.children('img').attr('alt', `${img.title}`);
-        clone.children('p').text(img.description);
-        $('main').append(clone[0]);
-        clone.show();
-        
-        //adds each keyword to a list
-        if(!keywords.includes(img.keyword)) {
-          keywords.push(img.keyword);
-        }
-      });
-    
-      //creates a select option for each keyword from the list
-      keywords.forEach((keyword) => {
-        let template = $('option');
-        let clone = template.clone();
-    
-        clone.attr('value', keyword);
-        clone.text(keyword);
-        $('select').append(clone[0]);
-      });
-    });
+    getPageTwo();
   }
-})
-
-
+});
 
 //handles option selection event
 $('select').change((e) => {
@@ -146,3 +30,130 @@ $('select').change((e) => {
     }
   });
 });
+
+function getPageOne() {
+  let allImgObjs = [];
+  let keywords = ['all'];
+
+  function Img(image_url, title, description, keyword, horns) {
+    this.image_url = image_url;
+    this.title = title;
+    this.description = description;
+    this.keyword = keyword;
+    this.horns = horns;
+    allImgObjs.push(this);
+  }
+
+  console.log('Page 1 was clicked');
+  $.get('./data/page-1.json').done(data => {
+    data.forEach(element => {
+      new Img(element.image_url, element.title, element.description, element.keyword, element.horns);
+    });
+    console.log('image objects: ', allImgObjs);
+    console.log('keywords: ', keywords);
+
+    let template = $('#photo-template');
+    $('main').empty();
+    $('main').append(template[0]);
+    console.log(template);
+
+    let optionTemplate = $('option');
+    $('select').empty();
+    $('select').append(optionTemplate[0]);
+    console.log(optionTemplate);
+
+    //creates a new section for each image
+    allImgObjs.forEach((img) => {
+      let template = $('section');
+      let clone = template.clone();
+  
+      clone.attr('id', `${img.keyword}`);
+      clone.attr('class', 'all');
+      clone.children('h2').text(img.title);
+      clone.children('img').attr('src', `${img.image_url}`);
+      clone.children('img').attr('alt', `${img.title}`);
+      clone.children('p').text(img.description);
+      $('main').append(clone[0]);
+      clone.show();
+      
+      //adds each keyword to a list
+      if(!keywords.includes(img.keyword)) {
+        keywords.push(img.keyword);
+      }
+    });
+  
+    //creates a select option for each keyword from the list
+    keywords.forEach((keyword) => {
+      let template = $('option');
+      let clone = template.clone();
+  
+      clone.attr('value', keyword);
+      clone.text(keyword);
+      $('select').append(clone[0]);
+    });
+  });
+}
+
+function getPageTwo() {
+  console.log('Page 2 was clicked');
+  let allImgObjs = [];
+  let keywords = ['all'];
+
+  function Img(image_url, title, description, keyword, horns) {
+    this.image_url = image_url;
+    this.title = title;
+    this.description = description;
+    this.keyword = keyword;
+    this.horns = horns;
+    allImgObjs.push(this);
+  }
+
+  $.get('./data/page-2.json').done(data => {
+    data.forEach(element => {
+      new Img(element.image_url, element.title, element.description, element.keyword, element.horns);
+    });
+    console.log('image objects: ', allImgObjs);
+    console.log('keywords: ', keywords);
+
+    let template = $('#photo-template');
+    $('main').empty();
+    $('main').append(template[0]);
+    console.log(template);
+
+    let optionTemplate = $('option');
+    $('select').empty();
+    $('select').append(optionTemplate[0]);
+    console.log(optionTemplate);
+
+
+    //creates a new section for each image
+    allImgObjs.forEach((img) => {
+      let template = $('section');
+      let clone = template.clone();
+  
+      clone.attr('id', `${img.keyword}`);
+      clone.attr('class', 'all');
+      clone.children('h2').text(img.title);
+      clone.children('img').attr('src', `${img.image_url}`);
+      clone.children('img').attr('alt', `${img.title}`);
+      clone.children('p').text(img.description);
+      $('main').append(clone[0]);
+      clone.show();
+      
+      //adds each keyword to a list
+      if(!keywords.includes(img.keyword)) {
+        keywords.push(img.keyword);
+      }
+    });
+  
+    //creates a select option for each keyword from the list
+    keywords.forEach((keyword) => {
+      let template = $('option');
+      let clone = template.clone();
+  
+      clone.attr('value', keyword);
+      clone.text(keyword);
+      $('select').append(clone[0]);
+    });
+  });
+}
